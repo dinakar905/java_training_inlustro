09fe3e9082a6a61b5a5edd3e4607e502d3b111f1
dinakar <prsdinakarpondy@gmail.com>
2026-06-11 09:24:22 +0530

    DailyTemperatures

--- src/monday9/DailyTemperatures.java
+++ src/monday9/DailyTemperatures.java
@@ -3,32 +3,23 @@ package monday9;
 import java.util.*;
 
 public class DailyTemperatures {
-    public static int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>(); // stores indices
+     public static void main(String[] args) {
+            int[] temps = {73, 74, 75, 71, 69, 72, 76, 73};
+            int n = temps.length;
+            int[] ans = new int[n];
+            Stack<Integer> stack = new Stack<>(); // stores indices
 
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
-                result[prevIndex] = i - prevIndex;
+            for (int i = 0; i < n; i++) {
+                while (!stack.isEmpty() && temps[i] > temps[stack.peek()]) {
+                    int prev = stack.pop();
+                    ans[prev] = i - prev; // days waited
+                }
+                stack.push(i);
             }
-            stack.push(i);
-        }
-        return result;
-    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] temperatures = new int[n];
-        for (int i = 0; i < n; i++) {
-            temperatures[i] = sc.nextInt();
+            // Print result
+            for (int val : ans) {
+                System.out.print(val + " ");
+            }
         }
-        int[] result = dailyTemperatures(temperatures);
-        for (int r : result) {
-            System.out.print(r + " ");
-        }
-        sc.close();
     }
-}
