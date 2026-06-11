1ccd1900db8b76a86f949b965f91edd90f647f74
dinakar <prsdinakarpondy@gmail.com>
2026-06-11 08:55:47 +0530

    DailyTemperatures

--- src/monday9/DailyTemperatures.java
+++ src/monday9/DailyTemperatures.java
@@ -1,11 +1,13 @@
 package monday9;
 
 import java.util.*;
+
 public class DailyTemperatures {
     public static int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>(); // stores indices
+
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int prevIndex = stack.pop();
